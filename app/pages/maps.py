aec332d9a37fbb2f4718047b1079003f9068dc88
andre-ls <andre.luis.e3@gmail.com>
2024-11-11 22:54:59 -0300

    Finalização de Filtros

--- app/pages/maps.py
+++ app/pages/maps.py
@@ -7,6 +7,7 @@ st.title('Mapas')
 with st.sidebar:
     st.title('Filtros')
     data = st.session_state['data']
-    date_min, date_max = st.select_slider('Data', options=data['Datetime'].sort_values(), value=[data['Datetime'].min(),data['Datetime'].max()])
+    date_min, date_max = st.select_slider('Data', options=data['Date'].sort_values(), value=[data['Date'].min(),data['Date'].max()])
     states = st.multiselect('State', data['State'].unique(), default=None)
+    magnitudes = st.select_slider('Magnitude',options=data['Magnitude'].sort_values().unique())
 
