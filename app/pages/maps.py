80d5921446619e4d9fc57414dfffc666e13ae072
andre-ls <andre.luis.e3@gmail.com>
2024-11-13 21:01:54 -0300

    Adição de Filtro de View de Mapa e Modularização de Código

--- app/pages/maps.py
+++ app/pages/maps.py
@@ -4,16 +4,26 @@ import numpy as np
 from keplergl import KeplerGl
 from streamlit_keplergl import keplergl_static
 
-st.title('Mapas')
-
-with st.sidebar:
-    st.title('Filtros')
-    data = st.session_state['data']
+def setupFilters(data):
+    """
+    Função responsável por configurar os filtros presentes na página de visualização de Mapas.
+    """
     date_min, date_max = st.select_slider('Data', options=data['Date'].sort_values(), value=[data['Date'].min(),data['Date'].max()])
-    states = st.multiselect('State', data['State'].unique(), default=None)
+    states = st.multiselect('Estado', data['State'].unique(), default=None)
     mag_min, mag_max = st.select_slider('Magnitude',options=data['Magnitude'].sort_values().unique(),value=[data['Magnitude'].min(),data['Magnitude'].max()])
+    map_view = st.radio('Tipo de Mapa',options=['Trajetórias','Colunas'])
+
+    if map_view == 'Colunas':
+        measure = st.selectbox('Medida a ser exibida', options=['Total de Tornados','Perdas Econômicas','Fatalidades','Feridos'])
+    else:
+        measure = None
+
+    return date_min, date_max, states, mag_min, mag_max, measure
 
 def filterData(data, date_min, date_max, mag_min, mag_max, states):
+    """
+    Função para Filtrar os dados a serem exibidos na página de visualização de Mapas.
+    """
     plot_data = data[(data['Date'] >= date_min) & (data['Date'] <= date_max)]
     plot_data = plot_data[(data['Magnitude'] >= mag_min) & (data['Magnitude'] <= mag_max)]
 
@@ -21,6 +31,12 @@ def filterData(data, date_min, date_max, mag_min, mag_max, states):
         plot_data = plot_data[data['State'].isin(states)]
 
     return plot_data
+
+st.title('Mapas')
+with st.sidebar:
+    st.title('Filtros')
+    data = st.session_state['data']
+    date_min, date_max, states, mag_min, mag_max, measure = setupFilters(data)
 
 mapData = filterData(data, date_min, date_max, mag_min, mag_max, states)
 
